7c2d92908b0e2f0df6b67757fbfdc2c57e87632b
Gerard24PI <gerard.perez@students.salle.url.edu>
2021-03-06 00:39:53 +0100

    hello adsld

---
+++ uOnes ADS ACs/AC1_0_wrk/workspace.ads
@@ -0,0 +1,37 @@
+<ADSWorkspace Revision="4" Version="100">
+    <Workspace Name="">
+        <LibraryDefs Name="lib.defs" />
+        <ConfigFile Name="dds.cfg" />
+        <ConfigFile Name="de_sim.cfg" />
+        <ConfigFile Name="hpeesofsim.cfg" />
+        <Log Name="netlist.log" />
+        <Log Name="search_history.log" />
+        <Preferences Name="Ex1_LTs_Paralel_lib_lay.prf" />
+        <Preferences Name="layout.prf" />
+        <Preferences Name="schematic.prf" />
+        <Dataset Name="ex1.ds" />
+        <Data_Files Name="ex1.ds" />
+        <Data_Files Name="ex1_data\logFile.txt" />
+        <Library Name="ads_standard_layers" />
+        <Library Name="ads_schematic_layers" />
+        <Library Name="empro_standard_layers" />
+        <Library Name="ads_builtin" />
+        <Library Name="ads_standard_layers_ic" />
+        <Library Name="ads_schematic_layers_ic" />
+        <Library Name="ads_schematic_ports_ic" />
+        <Library Name="ads_rflib" />
+        <Library Name="ads_sources" />
+        <Library Name="ads_simulation" />
+        <Library Name="ads_tlines" />
+        <Library Name="ads_bondwires" />
+        <Library Name="ads_datacmps" />
+        <Library Name="ads_behavioral" />
+        <Library Name="ads_textfonts" />
+        <Library Name="ads_common_cmps" />
+        <Library Name="ads_designs" />
+        <Library Name="ads_pelib" />
+        <Library Name="Ex1_LTs_Paralel_lib" />
+        <Data_Display Name="Simulacions.dds" />
+        <Cell Name="Ex1_LTs_Paralel_lib:AC1_0" />
+    </Workspace>
+</ADSWorkspace>
